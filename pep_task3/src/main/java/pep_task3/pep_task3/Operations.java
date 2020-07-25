17fe7b96fda07a6e52a96f1ac0cff2d0be11281f
akhi02 <shiva.akshith02@gmail.com>
2020-07-25 17:08:57 +0530

    task3

---
+++ pep_task3/src/main/java/pep_task3/pep_task3/Operations.java
@@ -0,0 +1,20 @@
+package pep_task3.pep_task3;
+
+public class Operations {
+	public void add(int a,int b)
+	{
+		System.out.println(a+b);
+	}
+	public void subtract(int a,int b)
+	{
+		System.out.println(a-b);
+	}
+	public void divide(int a,int b)
+	{
+		System.out.println(a/b);
+	}
+	public void multiply(int a,int b)
+	{
+		System.out.println(a*b);
+	}
+}
